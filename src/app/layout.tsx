4357aa8b09f33a44211bf6b06fde2060ef1df9a6
DemwE <mateuszczarnecki360@gmail.com>
2024-04-07 12:23:22 +0200

    Updated toaster colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,35 @@
-import type { Metadata } from "next";
+import type {Metadata} from "next";
 import "./main.css";
 import lang from "@/utils/lang/en.json";
 import {Providers} from "./providers";
-import { Toaster } from "sonner";
+import {Toaster} from "sonner";
 
 export const metadata: Metadata = {
   title: "CarMitsu",
   description: lang.description,
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
+export default function RootLayout({children,}: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en" className='dark scroll-smooth'>
     <body>
-      <Providers>
-        {children}
-      </Providers>
+    <Providers>
+      {children}
+    </Providers>
     </body>
-    <Toaster expand={false} position="top-center" richColors/>
+    <Toaster
+      expand={false}
+      position="top-center"
+      toastOptions={{
+        classNames: {
+          success: 'bg-success-400 text-gray-900 border border-0',
+          error: 'bg-danger-400 text-gray-900 border border-0',
+          warning: 'bg-warning-400 text-gray-900 border border-0',
+        },
+      }}
+    />
     </html>
   );
 }
