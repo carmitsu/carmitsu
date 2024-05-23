76815fd1de01d4aa95793c4cde8494e0b060f79a
DemwE <mateuszczarnecki360@gmail.com>
2024-05-23 17:02:36 +0200

    Updated realizations section

--- src/components/realizations/allRealizations.tsx
+++ src/components/realizations/allRealizations.tsx
@@ -11,7 +11,7 @@ export default function AllRealizations({realizations, realizationsData}: Lang &
     <section className="px-6 md:px-14 pb-3">
       <div className="space-y-4">
         <h1 className="max-md:text-2xl md:text-3xl lg:text-4xl">{realizations?.title}</h1>
-        <div className="grid gap-4 lg:grid-cols-2 xl:grid-cols-3">
+        <div className="flex flex-col lg:grid gap-4 lg:grid-cols-2 xl:grid-cols-3">
           {realizationsData.map((realization: any, index: number) => {
             return (
               <Realization key={index} image={realization.image} title={realization.title}
